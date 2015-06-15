75203c2e75b86e4c2e2909429bc68412c6affb90
virusnik <seryowka@gmail.com>
2015-06-15 14:21:13 +0600

    add animation on splashfragment

--- app/src/main/java/com/mirsoft/easyfixmaster/SplashActivityFragment.java
+++ app/src/main/java/com/mirsoft/easyfixmaster/SplashActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.RelativeLayout;
 
 
 /**
@@ -18,6 +23,65 @@ public class SplashActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_splash, container, false);
+        View view = inflater.inflate(R.layout.fragment_splash, container, false);
+        RelativeLayout splashLayout = (RelativeLayout)view.findViewById(R.id.layoutSplash);
+        Animation animation = new TranslateAnimation(1500,0,0,1000);
+        animation.setDuration(1500);
+        splashLayout.startAnimation(animation);
+        ///slideToLeft(splashLayout);
+        return view;
     }
+
+
+   /* // To animate view slide out from left to right
+    public void slideToRight(View view){
+        TranslateAnimation animate = new TranslateAnimation(0,view.getWidth(),0,0);
+        animate.setDuration(500);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+        view.setVisibility(View.VISIBLE);
+    }
+    // To animate view slide out from right to left
+    public void slideToLeft(View view){
+        TranslateAnimation animate = new TranslateAnimation(0,-view.getWidth(),0,0);
+        animate.setDuration(500);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+        view.setVisibility(View.VISIBLE);
+    }
+
+    // To animate view slide out from top to bottom
+    public void slideToBottom(View view){
+        TranslateAnimation animate = new TranslateAnimation(0,0,0,view.getHeight());
+        animate.setDuration(500);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+        view.setVisibility(View.GONE);
+    }
+
+    // To animate view slide out from bottom to top
+    public void slideToTop(View view){
+        TranslateAnimation animate = new TranslateAnimation(0,0,0,-view.getHeight());
+        animate.setDuration(500);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+        view.setVisibility(View.GONE);
+    }*/
+
+  /*  public class ButtonActivity extends MainActivity {
+        //Button View
+
+        Button btnLogin = null;
+        Button btnSignup = null;
+
+        public ButtonActivity() {
+            btnLogin = (Button) btnLogin.findViewById(View.VISIBLE);
+            btnSignup = (Button) btnSignup.findViewById(View.VISIBLE);
+        }
+    }*/
+
+// устанавливаем один обработчик для всех кнопок
+/*        btnLogin.setOnClickListener(this);
+        btnSignup.setOnClickListener(this);*/
+
 }
