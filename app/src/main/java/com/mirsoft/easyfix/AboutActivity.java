0111c2146a6b7463912e60f66a9b9c8028a13826
virusnik <seryowka@gmail.com>
2015-08-14 17:20:31 +0600

    Cosmetic ui improvements@ @

--- app/src/main/java/com/mirsoft/easyfix/AboutActivity.java
+++ app/src/main/java/com/mirsoft/easyfix/AboutActivity.java
@@ -1,11 +1,13 @@
 package com.mirsoft.easyfix;
 
+import android.content.pm.PackageInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.mirsoft.easyfix.R;
 
@@ -19,6 +21,14 @@ public class AboutActivity extends AppCompatActivity {
 
         toolbar = (Toolbar)findViewById(R.id.order_create_toolbar);
         setSupportActionBar(toolbar);
+        try {
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            TextView info_version = (TextView) findViewById(R.id.info_version);
+            String info_version_text = getString(R.string.info_version, pInfo.versionName);
+            info_version.setText(info_version_text);
+        }
+        catch(Exception e){
+
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
@@ -29,6 +39,7 @@ public class AboutActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
-    }
+        }
 
+    }
 }
